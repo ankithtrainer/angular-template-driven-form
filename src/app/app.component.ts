96308fe1931ee8bd7056f344a67658a6ea197274
ankithtrainer <ankith.trainer@gmail.com>
2024-10-19 21:03:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'templatedrivenform';
+  name: string = 'John Doe';
+  email: string = 'john.doe@example.com';
+
+  onSubmit(form: NgForm) {
+    // Form submission logic
+    console.log(form.value);
+  
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';        //import FormsModule
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule                    //Add in Imports Array
   ],
   providers: [],
   bootstrap: [AppComponent]
